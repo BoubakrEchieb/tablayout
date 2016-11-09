46665195f5023d7c5a27939249239f4506832b76
BE <BoubakrEchieb>
2016-11-09 15:52:16 +0000

    m

---
+++ app/src/main/java/enis/info/tabbedpages/DummyFragment.java
@@ -0,0 +1,34 @@
+package enis.info.tabbedpages;
+
+import android.annotation.SuppressLint;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by be on 09/11/16.
+ */
+public class DummyFragment extends Fragment{
+    int color;
+
+    public DummyFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dummy_fragment, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/enis/info/tabbedpages/ViewPagerAdapter.java
@@ -0,0 +1,47 @@
+package enis.info.tabbedpages;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by be on 09/11/16.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager supportFragmentManager) {
+        super(supportFragmentManager);
+    }
+
+    /**
+     * Return the Fragment associated with a specified position.
+     *
+     * @param position
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+    public void addFrag(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+
+    public void addFrag(DummyFragment dummyFragment,String title) {
+        mFragmentList.add(dummyFragment);
+        mFragmentTitleList.add(title);
+    }
+}
